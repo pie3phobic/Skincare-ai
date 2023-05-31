fb76840d505d8220d823e7701047c12ca11b63ec
= <nastya.minty@gmail.com>
2023-05-31 19:39:01 +0300

    added mobile adaptivity

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -41,7 +41,7 @@ export default function Dashboard() {
   };
 
   return (
-    <div className="max-w-7xl w-[800px] mx-auto px-4 sm:px-6 lg:px-8 py-12">
+    <div className="max-w-7xl w-[380px] lg:w-[800px] mx-auto px-4 sm:px-6 lg:px-8 py-12">
       <div className="">
         <div className="">
           <form onSubmit={(e) => handleSubmit(e)}>
